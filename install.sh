ac4506a2f8166c823b95a518664886d4fa6e6b1c
Aki <catmanett@gmail.com>
2021-07-26 20:59:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-xbps-install sway Waybar wofi wpgt pipewire zsh seatd mesa-dri
+xbps-install sway Waybar wofi wpgt pipewire zsh seatd mesa-dri kitty
 ln -s /etc/sv/seatd/ /var/services/
 sv up seatd
 
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+mkdir /home/albin/.config/
 
 ln -sb config/* /home/albin/.config/
 ln -sb .zshrc /home/albin/.zshrc
